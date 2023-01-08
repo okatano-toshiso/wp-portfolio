f01991f4c297552bb501a8c33a202dca8bed0469
okatano-toshiso <web@nuevo.jp>
2023-01-08 18:28:47 +0900

    root

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ssh-keygen -f "/home/runner/.ssh/known_hosts" -R "153.121.72.40"
 
-rsync -acvz --delete ./ alma@153.121.72.40:/var/www/html/
+rsync -acvz --delete ./ root@153.121.72.40:/var/www/html/
