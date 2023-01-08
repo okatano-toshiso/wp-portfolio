719241563128ca32932157afda140b883cd72f5c
okatano-toshiso <web@nuevo.jp>
2023-01-08 18:33:32 +0900

    root

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ssh-keygen -f "/home/runner/.ssh/known_hosts" -R "153.121.72.40"
+# ssh-keygen -f "/home/runner/.ssh/known_hosts" -R "153.121.72.40"
 
 rsync -acvz --delete ./ root@153.121.72.40:/var/www/html/
